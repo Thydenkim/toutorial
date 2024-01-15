1e56029c95a015790618598b916872fed67d1a98
Thyden <156598625+Thydenkim@users.noreply.github.com>
2024-01-15 10:19:45 +0700

    Add files via upload

---
+++ Examtestm6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examtestm6;
+
+/**
+ *
+ * @author THIDEN
+ */
+public class Examtestm6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
